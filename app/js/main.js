2f896d55162d6acfa9f4a357a6d2fa1337f12a92
HayleyHaggard <nastena_panova2000@mail.ru>
2022-07-15 14:59:13 +0300

    added themes/ fixed

--- app/js/main.js
+++ app/js/main.js
@@ -16,5 +16,33 @@ $(function () {
     once: true,
   });
 
+  const themeBtn = document.getElementById('theme-btn')
+  const app = document.getElementById('app-root')
+
+  function chosenTheme() {
+    if (localStorage.getItem('theme') === 'theme-dark') {
+      app.classList.add('theme-dark');
+    }
+    if (localStorage.getItem('theme') === 'theme-light') {
+      app.classList.add('theme-light');
+    }
+  }
+
+  const themeChange = () => {
+    if (app.classList.contains('theme-light')) {
+      app.classList.remove('theme-light');
+      app.classList.add('theme-dark');
+      localStorage.removeItem('theme', 'theme-light')
+      localStorage.setItem('theme', 'theme-dark')
+    } else {
+      app.classList.remove('theme-dark');
+      app.classList.add('theme-light');
+      localStorage.removeItem('theme', 'theme-dark')
+      localStorage.setItem('theme', 'theme-light')
+    }
+  }
+
+  themeBtn.addEventListener('click', themeChange)
+  chosenTheme()
 });
 
